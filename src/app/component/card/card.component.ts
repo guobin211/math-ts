01e897ca52ab2092ed379d2b040e7c751b036ad0
eloven <guobin201314@gmail.com>
2018-07-27 18:27:35 +0800

    change： 卡片视图
    Signed-off-by: eloven <guobin201314@gmail.com>

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
+import {NzMessageService} from "ng-zorro-antd";
+
 
 @Component({
   selector: 'app-card',
@@ -6,11 +9,69 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  newsData;
+  url = "http://127.0.0.1:8000/api/news";
+  isVisible = false;
+  isOkLoading = false;
+  modalTitle;
+  modalContent = [];
 
-  constructor() {
+  constructor(private http: Http, private nzMessageService: NzMessageService) {
   }
 
   ngOnInit() {
+    this.http.get(this.url).subscribe(
+      (data) => {
+        /** data 响应体 **/
+        console.log(data);
+        this.newsData = JSON.parse(data['_body']).result.data;
+        console.log(this.newsData);
+      },
+      () => {
+      },
+      () => {
+      }
+    )
+  }
+
+  showModal(i): void {
+    this.modalTitle = i.title;
+    this.modalContent.push(i.thumbnail_pic_s);
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    this.isOkLoading = true;
+    window.setTimeout(() => {
+      this.isVisible = false;
+      this.isOkLoading = false;
+    }, 3000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  doSeting(i) {
+    console.log(i);
+  }
+
+  doComment(i) {
+    this.modalTitle = '';
+    this.modalContent = [];
+    this.showModal(i);
+  }
+
+  doShare() {
+
+  }
+
+  cancel(): void {
+    this.nzMessageService.info('取消操作');
+  }
+
+  confirm(): void {
+    this.nzMessageService.info('确认取消订阅');
   }
 
 }
