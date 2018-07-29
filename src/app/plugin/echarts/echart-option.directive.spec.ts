80ece9c6f54a70a94156c2b9c3515d0cba8283ab
eloven <guobin201314@gmail.com>
2018-07-29 13:13:36 +0800

    add: editor-md plugin
    Signed-off-by: eloven <guobin201314@gmail.com>

---
+++ src/app/plugin/echarts/echart-option.directive.spec.ts
@@ -0,0 +1,8 @@
+import {EchartOptionDirective} from './echart-option.directive';
+
+describe('EchartOptionDirective', () => {
+  it('should create an instance', () => {
+    const directive = new EchartOptionDirective();
+    expect(directive).toBeTruthy();
+  });
+});
