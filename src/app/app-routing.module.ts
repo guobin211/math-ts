25352df2d13fc0ef4ebdd77425fd4bbfc9b888a3
eloven <guobin201314@gmail.com>
2018-09-16 05:49:17 +0800

    gui添加模块
    Signed-off-by: eloven <guobin201314@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,63 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LayoutComponent} from "./layout/layout.component";
-import {DashboardComponent} from "./pages/dashboard/dashboard.component";
-import {LoginComponent} from "./pages/login/login.component";
-import {Code404Component} from "./pages/code404/code404.component";
-import {ButtonComponent} from "./component/button/button.component";
-import {IconComponent} from "./component/icon/icon.component";
-import {RegisterComponent} from "./pages/register/register.component";
-import {FormComponent} from "./component/form/form.component";
-import {StepsComponent} from "./component/steps/steps.component";
-import {AnimateComponent} from "./component/animate/animate.component";
-import {FeedbackComponent} from "./component/feedback/feedback.component";
-import {TableComponent} from "./component/table/table.component";
-import {TableajaxComponent} from "./component/tableajax/tableajax.component";
-import {QuilleditComponent} from "./plugin/quilledit/quilledit.component";
-import {CascadeComponent} from "./component/cascade/cascade.component";
-import {CheckboxComponent} from "./component/checkbox/checkbox.component";
-import {CalendarComponent} from "./component/calendar/calendar.component";
-import {CardComponent} from "./component/card/card.component";
-import {EchartsComponent} from "./plugin/echarts/echarts.component";
-import {VideoComponent} from "./plugin/video/video.component";
-import {ValidatorsComponent} from "./plugin/validators/validators.component";
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
-import {ChinacityComponent} from "./component/chinacity/chinacity.component";
-import {EditormdComponent} from "./plugin/editormd/editormd.component";
-import {NgxEchartsComponent} from "./plugin/ngx-echarts/ngx-echarts.component";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
+
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {
-    path: 'home', component: LayoutComponent, children: [
-      {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-      // dashboard默认主页  在路由配置中传递data数据
-      {path: 'dashboard', component: DashboardComponent, data: [{ulName: '仪表盘'}]},
-      // component组件部分
-      {path: 'button', component: ButtonComponent},
-      {path: 'icon', component: IconComponent},
-      {path: 'form', component: FormComponent},
-      {path: 'steps', component: StepsComponent},
-      {path: 'animate', component: AnimateComponent},
-      {path: 'feedback', component: FeedbackComponent},
-      {path: 'table', component: TableComponent, data: {keep: true}},
-      {path: 'ajaxTable', component: TableajaxComponent},
-      {path: 'quillEdit', component: QuilleditComponent, data: {keep: true}},
-      {path: 'editorMd', component: EditormdComponent, data: {keep: true}},
-      {path: 'cascade', component: CascadeComponent},
-      {path: 'city', component: ChinacityComponent},
-      {path: 'checkbox', component: CheckboxComponent},
-      {path: 'calendar', component: CalendarComponent},
-      {path: 'card', component: CardComponent},
-      {path: 'charts', component: EchartsComponent},
-      {path: 'video', component: VideoComponent},
-      {path: 'validators', component: ValidatorsComponent},
-      {path: 'ngxcharts', component: NgxEchartsComponent}
-    ]
-  },
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: '**', component: Code404Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,101 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
-import {HashLocationStrategy, LocationStrategy, registerLocaleData} from '@angular/common';
+
+import { NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import { HashLocationStrategy, LocationStrategy, registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
 registerLocaleData(zh);
-import {MultiplePipe} from './pipes/multiple.pipe';
-import {LayoutComponent} from './layout/layout.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {Code404Component} from './pages/code404/code404.component';
-import {LoginComponent} from './pages/login/login.component';
-import {ButtonComponent} from './component/button/button.component';
-import {IconComponent} from './component/icon/icon.component';
-import {RegisterComponent} from './pages/register/register.component';
-import {FormComponent} from './component/form/form.component';
-import {StepsComponent} from './component/steps/steps.component';
-import {AnimateComponent} from './component/animate/animate.component';
-import {FeedbackComponent} from './component/feedback/feedback.component';
-import {TableComponent} from './component/table/table.component';
-import {TableajaxComponent} from './component/tableajax/tableajax.component';
-import {QuilleditComponent} from './plugin/quilledit/quilledit.component';
-import {QuillModule} from "ngx-quill";
-import {HttpModule, JsonpModule} from "@angular/http";
-import {CascadeComponent} from './component/cascade/cascade.component';
-import {CheckboxComponent} from './component/checkbox/checkbox.component';
-import {CalendarComponent} from './component/calendar/calendar.component';
-import {CardComponent} from './component/card/card.component';
-import {EchartsComponent} from './plugin/echarts/echarts.component';
-import {VideoComponent} from './plugin/video/video.component';
-import {VgCoreModule} from "videogular2/core";
-import {VgControlsModule} from "videogular2/controls";
-import {VgOverlayPlayModule} from "videogular2/overlay-play";
-import {VgBufferingModule} from "videogular2/buffering";
-import {ValidatorsComponent} from './plugin/validators/validators.component';
-import {EchartOptionDirective} from './plugin/echarts/echart-option.directive';
-import {NgAnimateComponent} from './component/animate/ng-animate/ng-animate.component';
-import {RouteReuseStrategy} from "@angular/router";
-import {AppRoutingCache} from "./app-routing-cache";
-import { ChinacityComponent } from './component/chinacity/chinacity.component';
-import { EditormdComponent } from './plugin/editormd/editormd.component';
-import { EditorMdDirective } from './plugin/editormd/editor-md.directive';
-import { NgxEchartsComponent } from './plugin/ngx-echarts/ngx-echarts.component';
-import {NgxEchartsModule} from "ngx-echarts";
+
+import { RouteReuseStrategy } from "@angular/router";
+import { AppRoutingCache } from "./app-routing-cache";
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core/core.module";
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultiplePipe,
-    LayoutComponent,
-    DashboardComponent,
-    Code404Component,
-    LoginComponent,
-    ButtonComponent,
-    IconComponent,
-    RegisterComponent,
-    FormComponent,
-    StepsComponent,
-    AnimateComponent,
-    FeedbackComponent,
-    TableComponent,
-    TableajaxComponent,
-    QuilleditComponent,
-    CascadeComponent,
-    CheckboxComponent,
-    CalendarComponent,
-    CardComponent,
-    VideoComponent,
-    ValidatorsComponent,
-    EchartsComponent,
-    EchartOptionDirective,
-    NgAnimateComponent,
-    ChinacityComponent,
-    EditormdComponent,
-    EditorMdDirective,
-    NgxEchartsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    HttpModule,
-    JsonpModule,
-    QuillModule,
-    BrowserModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule,
-    NgZorroAntdModule,
-    NgxEchartsModule
+    CoreModule,
+    SharedModule,
   ],
   providers: [
     {provide: NZ_I18N, useValue: zh_CN},
