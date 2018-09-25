dd35dd3e55c2606a27cbf968afbc2e46f3c4b984
eloven <guobin201314@gmail.com>
2018-09-25 15:07:28 +0800

    glib文件夹重命名

--- src/app/g/index.ts
+++
@@ -1,5 +0,0 @@
-import { GArray } from "./GArray";
-
-let chunk = GArray.chunk([1,2,3], 2);
-
-console.log(chunk);
---
+++ src/app/glib/index.ts
@@ -0,0 +1,5 @@
+import {GArray} from './GArray';
+
+let chunk = GArray.chunk([1,2,3], 2);
+
+console.log(chunk);
