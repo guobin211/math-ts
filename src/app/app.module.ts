05b9d13eb5470c9251b17866f1365288ce24978d
eloven <guobin201314@gmail.com>
2018-09-25 16:09:52 +0800

    测试lodash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LodashComponent} from './three/lodash/lodash.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, LodashComponent
   ],
   imports: [
     BrowserModule,
