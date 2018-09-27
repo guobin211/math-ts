15b51eaea0edfc7f6897a3c1d827fc3c081f701a
eloven <guobin201314@gmail.com>
2018-09-27 23:35:32 +0800

    add 测试http和client的请求方法

--- src/app/component/test/test.component.ts
+++ src/app/component/test/test.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpService} from '../../service/http.service';
-import {HttpclientService} from '../../service/httpclient.service';
+import {HttpClientService} from '../../service/httpclient.service';
 
 @Component({
   selector: 'app-test',
@@ -8,14 +8,38 @@ import {HttpclientService} from '../../service/httpclient.service';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
-
-  constructor(private http: HttpService,
-              private client: HttpclientService) { }
+  // http完整的响应头
+  private responseHeaders;
+  // http响应type
+  private contentType;
+  constructor(private http: HttpService, private client: HttpClientService) {
+  }
 
   ngOnInit() {
-   this.http.getData('goods').subscribe(res => {
-     console.log(res);
-   });
+    // this.client.getData('aaa').subscribe( res => {
+    //   console.log(res);
+    // });
+
+    // 通过client获取响应信息
+    // this.client.getConfigResponse('http://localhost:3000/api/').subscribe(
+    //    resp => {
+    //      this.responseHeaders = resp.headers;
+    //      this.contentType = resp.headers.get('Content-Type');
+    //      console.log(this.contentType);
+    //    }
+    // );
+    //  this.client.postForm('aaa', {name: 'jack'}).subscribe( res => {
+    //    console.log(res);
+    //  });
+
+    this.client.postData('aaa', 'name=jack').subscribe(res => {
+      console.log(res);
+    });
+    // this.client.getData('aaa', {'name': 'jack', 'age': 18}).subscribe( res => {
+    //   console.log(res);
+    // });
+
+
   }
 
 }
