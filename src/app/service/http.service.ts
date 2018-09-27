496b734d07947a8a8d8e771884dffb837aab210e
eloven <guobin201314@gmail.com>
2018-09-27 23:35:08 +0800

    add 添加http和client的请求方法

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,17 +1,72 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+import {catchError, map} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  private config = {
-    'dev': 'http://127.0.0.1:8000/',
-    'prod': 'http://www.yeo.com/api/'
+  // 常用请求头
+  headers = {
+    url: new Headers({'Content-Type': 'application/x-www-form-urlencoded'}),
+    json: new Headers({'Content-Type': 'application/json'}),
+    form: new Headers({'Content-Type': 'multipart/form-data'}),
   };
-  private header = new Headers();
-  constructor(private http: Http) { }
-  getData(url: string) {
-    return this.http.get(this.config.dev+url).pipe();
+  // api配置
+  private config = {
+    'api': 'http://localhost:3000/api/', 'token': 'username-admin-token'
+  };
+
+  constructor (private http: Http) {
+  }
+
+  /**
+   * get请求
+   * @param url
+   */
+  getData (url: string): Observable<any> {
+    return this.http.get(this.config.api + url, {headers: this.headers.form}).pipe(map(res => res['_body']), catchError(this.handleError()));
+  }
+
+  /**
+   * post请求
+   * @param url
+   * @param body
+   */
+  postData (url: string, body: any): Observable<any> {
+    return this.http.post(this.config.api + url, body, {headers: this.headers.form})
+      .pipe(map(res => res['_body']), catchError(this.handleError()));
+  }
+
+  /**
+   * 更新数据
+   * @param url
+   * @param body
+   */
+  updateData (url: string, body: any): Observable<any> {
+    return this.http.put(this.config.api + url, body, {headers: this.headers.form}).pipe(map(res => res['_body']), catchError(this.handleError()));
+  }
+
+  /**
+   * 错误处理
+   * @param operation
+   * @param result
+   */
+  private handleError<T> (operation = 'http', result?: T) {
+    return (error: any): Observable<T> => {
+      // better job of transforming error for user consumption
+      this.logMessage(`${operation} failed,status: ${error.status}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /**
+   * log服务函数
+   * @param message
+   */
+  private logMessage (message: string) {
+    console.log(message);
   }
 }
--- src/app/service/httpclient.service.ts
+++ src/app/service/httpclient.service.ts
@@ -1,9 +1,94 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {Observable, throwError} from 'rxjs';
+import {Config} from 'protractor';
+import {Params} from '@angular/router';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'my-auth-token'
+  }),
+};
+const option = {
+  headers: new HttpHeaders({
+    'Authorization': 'my-auth-token'
+  }),
+};
 @Injectable({
   providedIn: 'root'
 })
-export class HttpclientService {
+export class HttpClientService {
+  // api配置
+  private config = {
+    'api': 'http://localhost:3000/api/', 'token': 'username-admin-token'
+  };
   constructor(private http: HttpClient) { }
+
+  /**
+   * postForm
+   * @param url
+   * @param body
+   */
+  postForm (url, body: Object): Observable<any> {
+    // 使用浏览器默认分隔符: WebKitFormBoundaryq2imgPJa8fBQhoq6
+    const form = new FormData();
+    const keys = Object.keys(body);
+    keys.forEach(el => {
+      form.append(el, body[el]);
+    });
+    return this.http.post(this.config.api + url, form, option)
+      .pipe(// map( res => res['_body']),
+        catchError(this.handleError));
+  }
+
+  /**
+   * postData
+   * @param url
+   * @param body
+   */
+  postData (url, body: string): Observable<any> {
+    return this.http.post(this.config.api + url, body, httpOptions)
+      .pipe(// map( res => res['_body']),
+        catchError(this.handleError));
+  }
+
+  /**
+   * get请求
+   * @param url
+   * @param params
+   */
+  getData (url: string, params?: Params) {
+    if (params) {
+      const options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'my-auth-token'
+        }), params: params,
+      };
+      return this.http.get(this.config.api + url, options);
+    } else {
+      return this.http.get(this.config.api + url);
+    }
+  }
+
+  /**
+   * 获取完整的http响应信息
+   * @param url
+   */
+  getConfigResponse (url: string): Observable<HttpResponse<Config>> {
+    return this.http.get<Config>(url, {observe: 'response'});
+  }
+
+  private handleError (error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
 }
