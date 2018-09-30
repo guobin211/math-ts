1a30931225f12160df3d2695bc4e06ac8f519e08
eloven <guobin201314@gmail.com>
2018-09-30 16:26:55 +0800

    add 增加测试路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { TestComponent} from './component/test/test.component';
-import { HomeComponent} from './page/home/home.component';
-import { MissComponent} from './page/miss/miss.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TestComponent} from './component/test/test.component';
+import {HomeComponent} from './page/home/home.component';
+import {MissComponent} from './page/miss/miss.component';
+import {RxjsComponent} from './component/rxjs/rxjs.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'test', pathMatch: 'full'},
-  {path: 'test', component: TestComponent},
+  {path: '', redirectTo: 'test', pathMatch: 'full'}, {
+    path: 'test',
+    component: TestComponent,
+    children: [{path: '', redirectTo: 'rxj', pathMatch: 'full'}, {path: 'rxj', component: RxjsComponent}]
+  },
   {path: 'home', component: HomeComponent},
   {path: '**', redirectTo: 'miss', pathMatch: 'full'},
   {path: 'miss', component: MissComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
+import {AppRoutingModule} from './app-routing.module';
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
 
-import { AppComponent} from './app.component';
-import { LodashComponent} from './three/lodash/lodash.component';
-import { TestComponent } from './component/test/test.component';
-import { HomeComponent } from './page/home/home.component';
-import { MissComponent } from './page/miss/miss.component';
+import {AppComponent} from './app.component';
+import {LodashComponent} from './three/lodash/lodash.component';
+import {TestComponent} from './component/test/test.component';
+import {HomeComponent} from './page/home/home.component';
+import {MissComponent} from './page/miss/miss.component';
+import {RxjsComponent} from './component/rxjs/rxjs.component';
 
 
 @NgModule({
@@ -17,8 +18,7 @@ import { MissComponent } from './page/miss/miss.component';
     AppComponent,
     LodashComponent,
     TestComponent,
-    HomeComponent,
-    MissComponent
+    HomeComponent, MissComponent, RxjsComponent
   ],
   imports: [
     BrowserModule,
