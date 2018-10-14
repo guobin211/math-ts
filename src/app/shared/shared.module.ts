8ab50fcb095d58d6ebef6e0f6f35015ba945419a
eloven <guobin201314@gmail.com>
2018-10-14 11:49:26 +0800

    add 共享模块

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule,
-    HttpModule,
+    CommonModule, HttpModule, HttpClientModule,
   ],
   declarations: [],
-  exports: [
-    CommonModule,
-    HttpModule
+  exports: [CommonModule, HttpModule, HttpClientModule,
   ]
 })
 export class SharedModule { }
