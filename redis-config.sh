3a9719b8df0c6f408ce02e00d14586221c568f3c
eloven <guobin201314@gmail.com>
2018-12-16 09:45:14 +0800

    redis-config

--- redis-config.sh
+++ redis-config.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 ### start redis
 
 ### default port http://127.0.0.1:6379
 
-> redis-server redis.conf
+redis-server redis.conf
 
 ### stop redis
 
-> redis-cli shutdown
+#> redis-cli shutdown
