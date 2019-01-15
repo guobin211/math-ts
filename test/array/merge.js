e1b53b50556a58723ee3a08178f1bc9f572d7c46
eloven <guobin201314@gmail.com>
2019-01-15 15:33:32 +0800

    合并两个有序数组

--- test/array/merge.js
+++ test/array/merge.js
@@ -7,23 +7,15 @@
  */
 var merge = function (nums1, m, nums2, n) {
 
-  nums1.splice(m, nums1.length);
-
   for (let i = 0; i < n; i++) {
-    nums1.push(nums2[i]);
+    nums1[m] = nums2[i];
+    m++;
   }
 
-  nums1 = nums1.filter(el => el !== 0);
+  nums1.sort(function (a, b) {
+    return a - b;
+  });
 
-  for (let i = 0; i < nums1.length - 1; i++) {
-    for (let j = i + 1; j < nums1.length; j++) {
-      if (nums1[i] > nums1[j]) {
-        let temp = nums1[i];
-        nums1[i] = nums1[j];
-        nums1[j] = temp;
-      }
-    }
-  }
 };
 
 
@@ -33,4 +25,6 @@ let nums1 = [1, 2, 3, 0, 0, 0],
   n = 3;
 
 
-console.log(merge(nums1, m, nums2, n));
+merge(nums1, m, nums2, n);
+
+console.log(nums1);
