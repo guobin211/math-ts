c389b36d24b8896cb51751ce1155fa67191f78a8
eloven <guobin201314@gmail.com>
2019-01-16 16:08:28 +0800

    单一数

---
+++ src/package.array/singleNumber.ts
@@ -0,0 +1,19 @@
+/**
+ * singleNumber
+ * @author guobin201314@gmail.com on 2019-01-16
+ */
+
+export const singleNumber = function (nums: number[]) {
+    for (let i = 0; i < nums.length; i++) {
+        const el = nums[i];
+        let temp = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (el === nums[j]) {
+                temp++;
+            }
+        }
+        if (temp < 2) {
+            return el;
+        }
+    }
+};
