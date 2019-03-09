739edf9930c050bd2a5ca11c4bde7264abb2441d
eloven <guobin201314@gmail.com>
2019-03-09 13:14:23 +0800

    queue

--- src/package.queue/Queue.ts
+++ src/package.queue/Queue.ts
@@ -1,3 +1,35 @@
 export class Queue {
-    constructor() {}
+
+    private readonly collection: any[];
+
+    constructor() {
+        this.collection = [];
+    }
+
+    enqueue(el: any){
+        this.collection.push(el);
+    }
+
+    dequeue(): any {
+        if (this.isEmpty()) {
+            throw new Error('empty queue not allowed dequeue')
+        } else {
+            return this.collection.shift();
+        }
+    }
+
+    front(): any {
+        return this.collection[0];
+    }
+
+    size(): number {
+        return this.collection.length;
+    }
+
+    isEmpty(): boolean {
+        return this.size() === 0;
+    }
 }
+
+
+
