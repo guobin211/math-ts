880d9ca89a0bd5e2d5b657d1f8645824c11fefa3
Bin Guo <guobin201314@gmail.com>
2019-04-17 19:32:13 +0800

    增加浏览器工具

--- doc/typescript/ZipValidator.ts
+++
@@ -1,9 +0,0 @@
-import { StringValidator } from "./module";
-
-export const numberRegexp = /^[0-9]+$/;
-
-export class ZipValidator implements StringValidator {
-    isAcceptable(s: string) {
-        return s.length === 5 && numberRegexp.test(s);
-    }
-}
