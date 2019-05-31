261b2cbd45fa5160d5aa8c3dfc3d514ba9aee00a
GuoBin <844922881@qq.com>
2019-05-31 17:39:44 +0800

    array fix

--- src/package.array/index.ts
+++ src/package.array/index.ts
@@ -1,78 +1,46 @@
-import {arrayDifference} from "./arrayDifference";
-import {chunk} from "./chunk";
-import {compact} from "./compact";
-import {countBy} from "./countBy";
-import {all} from "./all";
-import {allEqual} from "./allEqual";
-import {any} from "./any";
-import {countOccurrences} from "./countOccurrences";
-import {deepFlatten} from "./deepFlatten";
-import {difference} from "./difference";
-import {differenceWith} from "./differenceWith";
-import {dropElement} from "./dropElements";
-import {flatten} from "./flatten";
-import {filterNoUnique} from "./filterNoUnique";
-import {init2DArray} from "./init2DArray";
-import {intersection} from "./intersection";
-import {indexOfAll} from "./indexOfAll";
-import {everyNth} from "./everyNth";
-import {groupBy} from "./groupBy";
-import {isSorted} from "./isSorted";
-import {join} from "./join";
-import {forEachRight} from "./forEachRight";
-import {mapObject} from "./mapObject";
-import {longsItem} from "./longsItem";
-import {setArray} from "./setArray";
-import {occurrence} from "./occunrrence";
-import {pick} from "./pick";
-import {pull} from "./pull";
-import {similar} from "./similar";
-import {pullAtValue} from "./pullAtValue";
-import {pullAtIndex} from "./pullAtIndex";
-import {reducedFilter} from "./reducedFilter";
-import {union} from "./union";
-import {sampleSize} from "./sampleSize";
-import {shuffle} from "./shuffle";
-import {sample} from "./sample";
-
-/**
- * package.array
- */
-export default {
-    all: all,
-    allEqual: allEqual,
-    any: any,
-    arrayDifference: arrayDifference,
-    chunk: chunk,
-    compact: compact,
-    countBy: countBy,
-    countOccurrences: countOccurrences,
-    deepFlatten: deepFlatten,
-    difference: difference,
-    differenceWith: differenceWith,
-    dropElement: dropElement,
-    everyNth: everyNth,
-    filterNoUnique: filterNoUnique,
-    flatten: flatten,
-    forEachRight: forEachRight,
-    groupBy: groupBy,
-    indexOfAll: indexOfAll,
-    init2DArray: init2DArray,
-    intersection: intersection,
-    isSorted: isSorted,
-    join: join,
-    longsItem: longsItem,
-    mapObject: mapObject,
-    occurrence: occurrence,
-    pick: pick,
-    pull: pull,
-    pullAtIndex: pullAtIndex,
-    pullAtValue: pullAtValue,
-    reducedFilter: reducedFilter,
-    sample: sample,
-    sampleSize: sampleSize,
-    setArray: setArray,
-    shuffle: shuffle,
-    similar: similar,
-    union: union,
-}
+export * from "./all";
+export * from "./allEqual";
+export * from "./any";
+export * from "./arrayDifference";
+export * from "./arrayToCSV";
+export * from "./chunk";
+export * from "./compact";
+export * from "./countBy";
+export * from "./countOccurrences";
+export * from "./deepFlatten";
+export * from "./difference";
+export * from "./differenceWith";
+export * from "./dropElement";
+export * from "./everyNth";
+export * from "./filterNoUnique";
+export * from "./flatten";
+export * from "./forEachRight";
+export * from "./groupBy";
+export * from "./index";
+export * from "./indexOfAll";
+export * from "./init2DArray";
+export * from "./intToBytes2";
+export * from "./intersect";
+export * from "./intersection";
+export * from "./isSorted";
+export * from "./join";
+export * from "./longsItem";
+export * from "./mapObject";
+export * from "./maxProfit";
+export * from "./occurrence";
+export * from "./pick";
+export * from "./pull";
+export * from "./pullAtIndex";
+export * from "./pullAtValue";
+export * from "./reducedFilter";
+export * from "./rotate";
+export * from "./rotateMatrix";
+export * from "./sample";
+export * from "./sampleSize";
+export * from "./setArray";
+export * from "./shuffle";
+export * from "./similar";
+export * from "./singleNumber";
+export * from "./stringToUint8Array";
+export * from "./uint8ArrayToString";
+export * from "./union";
