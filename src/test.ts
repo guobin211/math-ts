7751d8cffd68b206729d2506e505ab8943e7a9b5
GuoBin <844922881@qq.com>
2019-06-09 21:00:19 +0800

    fix tslint

--- src/test.ts
+++ src/test.ts
@@ -3,9 +3,59 @@
  * @author guobin201314@gmail.com on 2019-05-31
  */
 
-import { mt } from './index';
+const res = {
+  code: 200,
+  data: {
+    page: 1,
+    list: [
+      {
+        name: 'jack',
+        age: 22
+      },
+      {
+        name: 'jack',
+        age: 22
+      }
+    ]
+  }
+};
 
-const arr = [1, 2, 3, 4, 5];
-const res = mt.all(arr, (num: number) => num > 0);
+class IRes {
+  code: number;
+  data: List[];
+  constructor() {
+    this.code = 0;
+    this.data = [];
+  }
+}
+class List {
+  name: string;
+  age: number;
+  constructor() {
+    this.age = 0;
+    this.name = '';
+  }
+}
 
-console.log(res);
+interface IList {
+  name: string;
+  age: number;
+}
+
+// 基本类型 string number null undefined object
+// object
+console.log(typeof res);
+// false
+console.log(res instanceof IRes);
+// true
+console.log(res instanceof Object);
+console.log(res.data.list instanceof Array);
+
+const vdata = new IRes();
+// true
+console.log(vdata instanceof IRes);
+
+const mock = {} as IList;
+console.log(mock as IList);
+mock.name = 'jack';
+console.log(mock);
