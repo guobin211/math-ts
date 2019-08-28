07175bc608b089466de851ec95db1e0be2356c6a
GuoBin <guobin201314@gmail.com>
2019-08-28 09:52:34 +0800

    v:1.0.9 add <object>: extend(), isValid()

--- doc/es6/1.基础/let.js
+++
@@ -1,9 +0,0 @@
-
-let age = 26;
-function getAge() {
-  let sex = '男';
-  console.log(age);
-}
-console.log(sex);
-getAge();
-console.log(sex);
--- doc/es6/3.字符串/teststr.js
+++
@@ -1,7 +0,0 @@
-const str = '这是一个测试的字符串sx';
-
-console.log(str.startsWith('这是'));
-
-console.log(str.endsWith('sx'));
-
-console.log(str.includes('字'));
