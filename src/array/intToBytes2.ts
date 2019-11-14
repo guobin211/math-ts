84aacb2ae45c97f0f6023b2ce7ef9b157f9b03ed
GuoBin <guobin201314@gmail.com>
2019-11-14 16:06:34 +0800

    fix: type

--- src/array/intToBytes2.ts
+++ src/array/intToBytes2.ts
@@ -2,7 +2,7 @@
  * intToBytes2
  * @author guobin201314@gmail.com on 2019-04-17
  */
-export function intToBytes2(n: number): Array<any> {
+export function intToBytes2(n: number): Array<number> {
     const bytes = [];
     for (let i = 0; i < 2; i++) {
         bytes[i] = n >> (8 - i * 8);
