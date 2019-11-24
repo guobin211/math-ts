e3cbd075adc816c823672255cba0ed9452e050be
GuoBin <guobin201314@gmail.com>
2019-11-24 14:48:40 +0800

    加载脚本

---
+++ src/web/loadStyle.ts
@@ -0,0 +1,14 @@
+export function loadStyle(url: string) {
+  return new Promise((resolve, reject) => {
+    if ((document.styleSheets as any).some((style: HTMLLinkElement) => style.href === url)) {
+      resolve()
+    } else {
+      const link = document.createElement('link')
+      link.rel = 'stylesheet'
+      link.href = url
+      link.onload = resolve
+      link.onerror = reject
+      document.head.appendChild(link)
+    }
+  })
+}
