b69f251263a1828541bba5329d6d1b7a118550d0
GuoBin <guobin201314@gmail.com>
2019-12-19 23:46:40 +0800

    format

--- src/hashmap/HashMap.ts
+++ src/hashmap/HashMap.ts
@@ -1,23 +1,25 @@
 export class HashMap<T> {
-    constructor() {}
+  constructor() {
+  }
 
-    hash(obj: T) {
+  hash(obj: T) {
 
+  }
+
+  private static _objToHash(obj: any) {
+    if (typeof obj === 'number') {
     }
+  }
 
-    private static _objToHash(obj: any) {
-        if (typeof obj === 'number') {
-        }
-    }
+  static toBinaryString(obj: string): string {
+    return window.atob(obj)
+  }
 
-    static toBinaryString(obj: string): string {
-        return window.atob(obj)
-    }
-
-    static toUTF16(sString: string) {
-        let aUTF16CodeUnits: any = new Uint16Array(sString.length);
-        Array.prototype.forEach.call(aUTF16CodeUnits, function (el, idx, arr) { arr[idx] = sString.charCodeAt(idx); });
-        // @ts-ignore
-        return window.btoa(String.fromCharCode.apply(null, new Uint8Array(aUTF16CodeUnits.buffer)));
-    }
+  static toUTF16(sString: string) {
+    let aUTF16CodeUnits: any = new Uint16Array(sString.length);
+    Array.prototype.forEach.call(aUTF16CodeUnits, function (el, idx, arr) {
+      arr[idx] = sString.charCodeAt(idx);
+    });
+    return window.btoa(String.fromCharCode.apply(null, new Uint8Array(aUTF16CodeUnits.buffer) as any));
+  }
 }
