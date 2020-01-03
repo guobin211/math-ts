32c7c5fb6351e0d826537a4bc6af0898ae7cb2f7
GuoBin <guobin201314@gmail.com>
2020-01-03 14:50:26 +0800

    Node and BSTTree

--- docs/guide/hashmap.md
+++ docs/guide/hashmap.md
@@ -1,1 +1,10 @@
 # HashMap
+```js
+    
+   const name = 'jack'
+   let char = 17
+   for (let i = 0; i < name.length; i++) {
+     char += char * 17 + name.charCodeAt(i)
+   }
+   
+```
--- src/tree/Node.ts
+++ src/tree/Node.ts
@@ -2,16 +2,11 @@
  * BST
  * @author guobin201314@gmail.com on 2019-03-09
  */
-export class Node<T> {
-
-  public data: T;
-  public left?: Node<T>;
-  public right?: Node<T>;
-
-  constructor(data: T, left?: Node<T>, right?: Node<T>) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
-  }
+export abstract class BaseNode<T> {
+  public readonly abstract id: string;
+  public readonly abstract parentId: string | null;
+  public readonly abstract children: BaseNode<T>[];
+  public readonly abstract data: T;
+  public abstract isRoot: () => boolean;
 }
 
