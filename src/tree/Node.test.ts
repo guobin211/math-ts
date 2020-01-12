4eb9f2d62d4c93a909f5c1e545cab60c47333f65
GuoBin <guobin201314@gmail.com>
2020-01-12 23:31:00 +0800

    tree

--- src/tree/Node.test.ts
+++ src/tree/Node.test.ts
@@ -1,65 +1,65 @@
-import { BaseNode } from './Node'
+import { BaseNode } from "./Node";
 
 class NumberNode extends BaseNode<number> {
-  readonly id: string
-  readonly parentId: string | null
-  readonly data: number
-  readonly children: BaseNode<number>[]
-  isRoot: () => boolean
+    readonly id: string;
+    readonly parentId: string | null;
+    readonly data: number;
+    readonly children: BaseNode<number>[];
+    isRoot: () => boolean;
 
-  constructor(id: string, data: number, parentId?: string) {
-    super();
-    this.id = id;
-    this.data = data;
-    this.parentId = parentId ? parentId : null
-    this.children = [];
-    this.isRoot = () => this.parentId === null
-  }
+    constructor(id: string, data: number, parentId?: string) {
+        super();
+        this.id = id;
+        this.data = data;
+        this.parentId = parentId ? parentId : null;
+        this.children = [];
+        this.isRoot = () => this.parentId === null;
+    }
 
 }
 
 class NodeManage {
 
-  private readonly _list: NumberNode[] = [];
+    private readonly _list: NumberNode[] = [];
 
-  public addElement(el: NumberNode) {
-    if (el.parentId === null) {
-      this._list.push(el)
-    } else {
-      const res = this._insertByParentId(el, this._list)
-      if (res === null) {
-        throw new Error(`${el.parentId} is not in collection`)
-      }
+    public addElement(el: NumberNode) {
+        if (el.parentId === null) {
+            this._list.push(el);
+        } else {
+            const res = this._insertByParentId(el, this._list);
+            if (res === null) {
+                throw new Error(`${el.parentId} is not in collection`);
+            }
+        }
     }
-  }
 
-  public findById(id: string) {
-    this._findById(id, this._list)
-  }
-
-  private _findById(id: string, list: NumberNode[]): NumberNode | null {
-    for (const item of list) {
-      if (item.id === id) {
-        return item
-      }
-      if (item.children.length > 0) {
-        this._findById(id, item.children)
-      }
+    public findById(id: string) {
+        this._findById(id, this._list);
     }
-    return null
-  }
 
-  private _insertByParentId( el: NumberNode, list: NumberNode[]): NumberNode | null {
-    for (const item of list) {
-      if (item.id === el.parentId) {
-        item.children.push(el)
-        return el
-      }
-      if (item.children.length > 0) {
-        this._insertByParentId(el, item.children)
-      }
+    private _findById(id: string, list: NumberNode[]): NumberNode | null {
+        for (const item of list) {
+            if (item.id === id) {
+                return item;
+            }
+            if (item.children.length > 0) {
+                this._findById(id, item.children);
+            }
+        }
+        return null;
     }
-    return null
-  }
+
+    private _insertByParentId(el: NumberNode, list: NumberNode[]): NumberNode | null {
+        for (const item of list) {
+            if (item.id === el.parentId) {
+                item.children.push(el);
+                return el;
+            }
+            if (item.children.length > 0) {
+                this._insertByParentId(el, item.children);
+            }
+        }
+        return null;
+    }
 
 }
--- src/tree/Tree.ts
+++ src/tree/Tree.ts
@@ -1,64 +1,64 @@
 export class Tree<T> {
-  public left: Tree<T> | null
-  public right: Tree<T> | null
-  public root?: Tree<T>
-  public list: Tree<T>[] = [];
-  private data: T;
+    public left: Tree<T> | null;
+    public right: Tree<T> | null;
+    public root?: Tree<T>;
+    public list: Tree<T>[] = [];
+    private data: T;
 
-  constructor(data: T, left?: Tree<T>, right?: Tree<T>) {
-    this.data = data
-    this.left = left ? left : null
-    this.right = right ? right : null
-  }
+    constructor(data: T, left?: Tree<T>, right?: Tree<T>) {
+        this.data = data;
+        this.left = left ? left : null;
+        this.right = right ? right : null;
+    }
 
-  public create(list: T[]) {
-    this.list = []
-    for (const el of list) {
-      this.list.push(new Tree<T>(el))
+    public create(list: T[]) {
+        this.list = [];
+        for (const el of list) {
+            this.list.push(new Tree<T>(el));
+        }
+        this.root = this.list[0];
+        for (let i = 0; i < list.length / 2; i++) {
+            this.list[i].left = this.list[i * 2 + 1];
+            if (i * 2 + 2 < this.list.length) {
+                this.list[i].right = this.list[i * 2 + 2];
+            }
+        }
     }
-    this.root = this.list[0];
-    for (let i = 0; i < list.length / 2; i++) {
-      this.list[i].left = this.list[i * 2 + 1]
-      if (i * 2 + 2 < this.list.length) {
-        this.list[i].right = this.list[i * 2 + 2]
-      }
-    }
-  }
 
-  /**
-   * 前序遍历
-   * @param root
-   */
-  public preorder(root: Tree<T> | null) {
-    if (root !== null) {
-      console.log(root.data)
-      this.preorder(root.left)
-      this.preorder(root.right)
+    /**
+     * 前序遍历
+     * @param root
+     */
+    public preorder(root: Tree<T> | null) {
+        if (root !== null) {
+            console.log(root.data);
+            this.preorder(root.left);
+            this.preorder(root.right);
+        }
     }
-  }
 
-  /**
-   * 中序遍历
-   * @param root
-   */
-  public inorder(root: Tree<T> | null) {
-    if (root !== null) {
-      this.preorder(root.left)
-      console.log(root.data)
-      this.preorder(root.right)
+    /**
+     * 中序遍历
+     * @param root
+     */
+    public inorder(root: Tree<T> | null) {
+        if (root !== null) {
+            this.preorder(root.left);
+            console.log(root.data);
+            this.preorder(root.right);
+        }
     }
-  }
 
-  /**
-   * 后序遍历
-   * @param root
-   */
-  public afterorder(root: Tree<T> | null) {
-    if (root !== null) {
-      console.log(root.data)
-      this.preorder(root.left)
-      this.preorder(root.right)
+    /**
+     * 后序遍历
+     * @param root
+     */
+    public afterorder(root: Tree<T> | null) {
+        if (root !== null) {
+            console.log(root.data);
+            this.preorder(root.left);
+            this.preorder(root.right);
+        }
     }
-  }
 
 }
