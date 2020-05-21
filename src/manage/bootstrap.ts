838c4f49138da66be5c1dcd7a10295a4f29f47a1
GuoBin <guobin201314@gmail.com>
2020-05-21 17:57:42 +0800

    feature(manage): app start interface

---
+++ src/manage/bootstrap.ts
@@ -0,0 +1,18 @@
+export interface Bootstrap {
+  /**
+   * init local state
+   */
+  initState: ()=> void;
+  /**
+   * init ajax state
+   */
+  initAjax: () => void;
+  /**
+   * update state
+   */
+  updateState: () => void;
+  /**
+   * init app component
+   */
+  initApp: () => void;
+}
